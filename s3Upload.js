61c45e58f4cf161af6b28c0046a797cbb6a74c6a
newmohib <newmohib@gmail.com>
2019-12-30 03:38:02 +0600

    initial commit

---
+++ s3Upload.js
@@ -0,0 +1,25 @@
+var AWS = require('aws-sdk'),
+    fs = require('fs');
+
+// For dev purposes only
+AWS.config.update({ accessKeyId: '',
+ secretAccessKey: '' });
+
+// Read in the file, convert it to base64, store to S3
+fs.readFile('./del.txt', function (err, data) {
+  if (err) { throw err; }
+
+  var base64data = new Buffer(data, 'binary');
+
+  var s3 = new AWS.S3();
+  s3.upload({
+    Bucket: 'abbvie-darma-dev',
+    Key: 'del2.txt',
+    Body: base64data,
+    ACL: 'public-read'
+  },function () {
+    console.log(arguments);
+    console.log('Successfully uploaded package.');
+  });
+
+});
